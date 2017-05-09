106d293070011dc5c4ab0c4769caa869b57b850e
Katie Koschland <katiekoschland@hotmail.com>
2017-05-09 12:16:14 +0100

    Grid now generates 441 cells upon instantiation

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,3 +1,20 @@
 class Grid
 
+  attr_reader :cells
+
+  def initialize
+    @cells = []
+    generate_grid
+  end
+
+  private
+
+  def generate_grid
+    [*-10..+10].each do |x|
+      [*-10..+10].each do |y|
+        @cells << Cell.new(x,y)
+      end
+    end
+  end
+
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,7 +2,12 @@ require 'grid'
 
 describe Grid do
   subject(:grid) { Grid.new }
+
   it 'can be instantiated' do
     expect(grid).to be_instance_of(Grid)
+  end
+
+  it 'has 441 cells' do
+    expect(grid.cells.length).to eq 441
   end
 end
