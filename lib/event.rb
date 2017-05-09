9caf19108923634e54a6d61c0ee440faaa85a516
Katie Koschland <katiekoschland@hotmail.com>
2017-05-09 15:09:33 +0100

    Event responds to unique_id, ticket_quantity and price($)

--- lib/event.rb
+++ lib/event.rb
@@ -1,2 +1,8 @@
 class Event
+
+  attr_reader :unique_id, :ticket_quantity, :price_in_dollars
+
+  def initialize
+  end
+
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,4 +6,9 @@ describe Event do
   it 'can be instantiated' do
     expect(event).to be_instance_of(Event)
   end
+
+  it 'responds to unique_id, ticket quantity and price($)' do
+    expect(event).to respond_to(:unique_id, :ticket_quantity, :price_in_dollars)
+  end
+
 end
