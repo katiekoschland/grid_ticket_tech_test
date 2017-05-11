e181cfd6ddc014ea625c389dc4c494ba8c5c9141
Katie Koschland <katiekoschland@hotmail.com>
2017-05-11 09:35:50 +0100

    All event coordinates and event IDS are now stored into seperate arrays

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,26 @@
+require 'json'
+
 class User
 
-end 
+  attr_reader :user_w_coord, :user_z_coord, :x_coord_events, :y_coord_events, :event_ids, :event_location
+
+  def initialize(w,z)
+    @user_w_coord = w
+    @user_z_coord = z
+    @x_coord_events = []
+    @y_coord_events = []
+    @event_ids = []
+    event_location
+  end
+
+
+  def event_location
+    file = File.read('event_data.json')
+    events = JSON.parse(file)
+    events.each do |event|
+      @x_coord_events << event["x_coord"]
+      @y_coord_events << event["y_coord"]
+      @event_ids << event["unique_id"]
+    end
+  end
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,21 @@
 require 'user'
 
 describe User do
-  subject(:user){ User.new }
+  subject(:user){ User.new(5,4) }
   it 'can be instantiated' do
     expect(user).to be_instance_of(User)
   end
+
+  it 'has the correct w coordinate' do
+    expect(user.user_w_coord).to eq(5)
+  end
+
+  it 'has the correct z coordinate' do
+    expect(user.user_z_coord).to eq(4)
+  end
+
+  it 'x_coord_events is not empty' do
+    expect(user.event_location.length).to eq(100)
+  end
+
 end
