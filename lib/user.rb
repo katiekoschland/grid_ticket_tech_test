3b21f16b97b7b083cc3d73f7e5849baab7d19f8f
Katie Koschland <katiekoschland@hotmail.com>
2017-05-11 14:08:26 +0100

    Corrected manhattan_distance method and refactored User class

--- lib/user.rb
+++ lib/user.rb
@@ -2,44 +2,29 @@ require 'json'
 
 class User
 
-  attr_reader :user_w_coord, :user_z_coord, :x_coord_events, :y_coord_events, :event_ids, :event_location, :manhattan_distance, :five_closest_events
+  attr_reader :user_w_coord, :user_z_coord, :manhattan_distance, :five_closest_events
 
   def initialize(w,z)
     @user_w_coord = w
     @user_z_coord = z
-    @x_coord_events = []
-    @y_coord_events = []
-    @event_ids = []
-    event_location
     @manhattan_distance = []
     manhattan_distance
     @five_closest_events = []
     five_closest_events
   end
 
-  def event_location
+  def manhattan_distance
     file = File.read('event_data.json')
     events = JSON.parse(file)
     events.each do |event|
-      @x_coord_events << event["x_coord"]
-      @y_coord_events << event["y_coord"]
-      @event_ids << event["unique_id"]
+      x_abs_dist =  (event["x_coord"] -  user_w_coord).abs
+      y_abs_dist =  (event["y_coord"] -  user_z_coord).abs
+      @manhattan_distance << x_abs_dist + y_abs_dist
     end
   end
 
   def five_closest_events
     @five_closest_events = @manhattan_distance.min(5)
-  end
-
-  private
-  def manhattan_distance
-    (0..@x_coord_events.length-1).each do |i|
-      (0..@y_coord_events.length-1).each do |j|
-        x_abs_dist =  (@x_coord_events[i] -  user_w_coord).abs
-        y_abs_dist =  (@y_coord_events[j] -  user_z_coord).abs
-        @manhattan_distance << x_abs_dist + y_abs_dist
-      end
-    end
   end
 
 end
