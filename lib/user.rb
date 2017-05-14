350834da5b712cee563097c8af562174607038d0
Katie Koschland <katiekoschland@hotmail.com>
2017-05-14 17:24:18 +0100

    welcome_viagogo,  manhattan_distance, five_closest_events, print_closest_events now private methods

--- lib/user.rb
+++ lib/user.rb
@@ -21,50 +21,52 @@ class User
     "Book now to avoid disappointment"
   end
 
-  def manhattan_distance
-    file = File.read('event_data.json')
-    events = JSON.parse(file)
-    events.each do |event|
-      x_abs_dist =  (event["x_coord"] -  user_w_coord.to_i).abs
-      y_abs_dist =  (event["y_coord"] -  user_z_coord.to_i).abs
-      @events <<  [event["unique_id"], event["ticket_price"], x_abs_dist + y_abs_dist]
-    end
-  end
-
-  def five_closest_events
-    @events_ordered_by_dist += @events.sort_by { |e| e[2] }
-    @five_closest_events += @events_ordered_by_dist.take(5)
-  end
-
-  def print_closest_events
-  puts "\n Event #{@five_closest_events[0][0]} - #{@five_closest_events[0][1]}, Distance: #{@five_closest_events[0][2]}"
-  puts "\n Event #{@five_closest_events[1][0]} - #{@five_closest_events[1][1]}, Distance: #{@five_closest_events[1][2]}"
-  puts "\n Event #{@five_closest_events[2][0]} - #{@five_closest_events[2][1]}, Distance: #{@five_closest_events[2][2]}"
-  puts "\n Event #{@five_closest_events[3][0]} - #{@five_closest_events[3][1]}, Distance: #{@five_closest_events[3][2]}"
-  puts "\n Event #{@five_closest_events[4][0]} - #{@five_closest_events[4][1]}, Distance: #{@five_closest_events[4][2]}"
-  end
+  private
 
   def welcome_viagogo
-   puts """\n Welcome to our online event locator with
-                            _________
+    puts """\n Welcome to our online event locator with
+                              _________
 
-                                              `/oo:
-                                             sMMMM.
-                                             .sdho                                                     `             ``
-                              +oooo     +ooo//ysss. ./syhhhys/.    `.://///:-.`    .-:////:.`     ./syhdhhyo:`   ./shddhy+-`
-                              /NMMMo   +MMMm.sMMMM- :NNmmmmNMMN/  :+ooo+++oooo/  -+ooo+++ooo+-  `sNMMMNmNMMMMs .yNMMNmmNMMMd-
-                               oMMMN- .NMMN: +MMMM-  :///+odMMMh /ooo+-```+ooo/ -oooo:```:oooo: hMMMN/...mMMMs`dMMMm:..:dMMMN.
-                                hMMMd`hMMM+  +MMMM- +dNMMNmNMMMh`oooo-    +ooo/ +ooo+     +ooo+.MMMMo    dMMMs/MMMM/    :MMMM/
-                                `mMMMdMMMy   +MMMM-/MMMh-..sMMMh`oooo/`   +ooo/ /ooo+`   `+ooo/`NMMMh`   mMMMs-MMMMs`   oMMMM:
-                                 -NMMMMMd`   +MMMM-+MMMd//+dMMMh -oooo+///oooo/ .+ooo+:-:+ooo+` oNMMMmhhdNMMMs oMMMMdsohNMMNo
-                                  /MMMMm.    +MMMM-`omMMMMNdMMMh  .:++ooo+oooo/  `:+oooooo+/-`   -ydNMMNmNMMMs  :ymMMMMMMmy:
-                                   ....`     `....   `-::-.`...`   ` `````oooo/     `....``      `.`.....NMMMs    `.://-.`
-                                                                 `/+/::::+oooo.                 .dNdysoymMMMN:
-                                                                 ./+ooooooo+:`                  /ymMMMMMMMms.
-                                                                    ``...``                        `.----`
+                                                `/oo:
+                                               sMMMM.
+                                               .sdho                                                     `             ``
+                                +oooo     +ooo//ysss. ./syhhhys/.    `.://///:-.`    .-:////:.`     ./syhdhhyo:`   ./shddhy+-`
+                                /NMMMo   +MMMm.sMMMM- :NNmmmmNMMN/  :+ooo+++oooo/  -+ooo+++ooo+-  `sNMMMNmNMMMMs .yNMMNmmNMMMd-
+                                 oMMMN- .NMMN: +MMMM-  :///+odMMMh /ooo+-```+ooo/ -oooo:```:oooo: hMMMN/...mMMMs`dMMMm:..:dMMMN.
+                                  hMMMd`hMMM+  +MMMM- +dNMMNmNMMMh`oooo-    +ooo/ +ooo+     +ooo+.MMMMo    dMMMs/MMMM/    :MMMM/
+                                  `mMMMdMMMy   +MMMM-/MMMh-..sMMMh`oooo/`   +ooo/ /ooo+`   `+ooo/`NMMMh`   mMMMs-MMMMs`   oMMMM:
+                                   -NMMMMMd`   +MMMM-+MMMd//+dMMMh -oooo+///oooo/ .+ooo+:-:+ooo+` oNMMMmhhdNMMMs oMMMMdsohNMMNo
+                                    /MMMMm.    +MMMM-`omMMMMNdMMMh  .:++ooo+oooo/  `:+oooooo+/-`   -ydNMMNmNMMMs  :ymMMMMMMmy:
+                                     ....`     `....   `-::-.`...`   ` `````oooo/     `....``      `.`.....NMMMs    `.://-.`
+                                                                   `/+/::::+oooo.                 .dNdysoymMMMN:
+                                                                   ./+ooooooo+:`                  /ymMMMMMMMms.
+                                                                      ``...``                        `.----`
 
 
-   """
-  end
+     """
+    end
+
+    def manhattan_distance
+      file = File.read('event_data.json')
+      events = JSON.parse(file)
+      events.each do |event|
+        x_abs_dist =  (event["x_coord"] -  user_w_coord.to_i).abs
+        y_abs_dist =  (event["y_coord"] -  user_z_coord.to_i).abs
+        @events <<  [event["unique_id"], event["ticket_price"], x_abs_dist + y_abs_dist]
+      end
+    end
+
+    def five_closest_events
+      @events_ordered_by_dist += @events.sort_by { |e| e[2] }
+      @five_closest_events += @events_ordered_by_dist.take(5)
+    end
+
+    def print_closest_events
+      puts "\n Event #{@five_closest_events[0][0]} - #{@five_closest_events[0][1]}, Distance: #{@five_closest_events[0][2]}"
+      puts "\n Event #{@five_closest_events[1][0]} - #{@five_closest_events[1][1]}, Distance: #{@five_closest_events[1][2]}"
+      puts "\n Event #{@five_closest_events[2][0]} - #{@five_closest_events[2][1]}, Distance: #{@five_closest_events[2][2]}"
+      puts "\n Event #{@five_closest_events[3][0]} - #{@five_closest_events[3][1]}, Distance: #{@five_closest_events[3][2]}"
+      puts "\n Event #{@five_closest_events[4][0]} - #{@five_closest_events[4][1]}, Distance: #{@five_closest_events[4][2]}"
+    end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -17,35 +17,8 @@ describe User do
       expect(user.events_ordered_by_dist).to eq([])
     end
 
-    it 'five_closest_events is empty' do
-      expect(user.five_closest_events).to eq([])
-    end
-
     it 'responds to closest events' do
       expect(user).to respond_to(:closest_events)
-    end
-  end
-
-  describe '#five_closest_events' do
-
-    before(:each) do
-      user_w_coord = 4
-      user_z_coord = 5
-    end
-
-    it 'manhattan_distance is calculated for the 100 events' do
-      expect(user.manhattan_distance.length).to eq(100)
-    end
-
-    it 'events are ordered by distance' do
-      user.manhattan_distance
-      user.five_closest_events
-      expect(user.events_ordered_by_dist.first).to eq([39, "$277.00", 1])
-    end
-
-    it 'has a length of 5' do
-      user.manhattan_distance
-      expect(user.five_closest_events.length).to eq(5)
     end
   end
 
@@ -53,9 +26,6 @@ describe User do
 
     it 'accepts coordinates as user input and returns five closest event' do
       allow(user).to receive(:gets).and_return("4 , 5")
-      user.welcome_viagogo
-      user.manhattan_distance
-      user.five_closest_events
       body = "Closest events to: 4, 5 \nEvent 39 -$277.00, Distance: 1\Event 41 -$164.14, Distance: 2\Event 50 -$169.47, Distance: 2\nEvent 65 -$29.92, Distance: 2\nEvent 66 -$281.28, Distance: 3"
       expect{user.closest_events}.to output{body}.to_stdout
     end
